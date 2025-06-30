c7824292706de7647a1c60257beb7832edf5c01d
biniam <biniamabayneh9@gmail.com>
2025-06-29 23:08:07 -0700

    function in python are committed

--- emoji_convertor.py
+++ emoji_convertor.py
@@ -1,10 +1,26 @@
+# message = input("> ")
+# words = message.split(" ")
+# emoji ={
+#     ":)" : "😁",
+#     ":(" : "😑"
+# }
+# output = ("")
+# for word in words:
+#     output +=emoji.get(word,word) + " "
+# print(output)
+
+
+def emoji_convertor(message) :
+    words = message.split(" ")
+    emoji = {
+        ":)": "😁",
+        ":(": "😑"
+    }
+    output = ("")
+    for word in words:
+        output += emoji.get(word, word) + " "
+    return output
 message = input("> ")
-words = message.split(" ")
-emoji ={
-    ":)" : "😁",
-    ":(" : "😑"
-}
-output = ("")
-for word in words:
-    output +=emoji.get(word,word) + " "
-print(output)
+
+
+print(emoji_convertor(message))
